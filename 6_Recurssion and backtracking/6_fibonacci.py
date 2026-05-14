64df685e9111aec244c2564a7be394399562c8f2
viveksen007 <46viveksen@gmail.com>
2026-05-14 20:30:52 +0530

    Recursion

---
+++ 6_Recurssion and backtracking/6_fibonacci.py
@@ -0,0 +1,11 @@
+def Fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+    
+if __name__ == "__main__":
+
+    print(Fibonacci(15))
